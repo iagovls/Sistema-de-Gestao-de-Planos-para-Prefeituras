497f12c5f939b5c91ff0a7f779275a15e3ab488d
Iago Vitor <iagovls@gmail.com>
2025-06-05 10:41:08 -0300

    first configuration and title home page

---
+++ front/front-sistema-de-gestao-prefeituras/src/app/components/header.tsx
@@ -0,0 +1,9 @@
+export function Header(){
+    return(
+        <div className=" w-full h-auto flex justify-between p-3 text-white">
+            <button className="bg-azulclaro w-32 h-10 rounded-2xl font-semibold">Início</button>
+            <button className="bg-azulclaro w-32 h-10 rounded-2xl font-semibold">Entrar</button>
+            
+        </div>
+    )
+}
---
+++ front/front-sistema-de-gestao-prefeituras/src/app/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import { Header } from "./components/header";
+import { Title } from "./components/title";
+
+
+export default function Home() {
+  return (
+    <div>
+      <Header/>
+      <Title/>
+      <main className="">
+        
+      </main>
+    </div>
+  );
+}
