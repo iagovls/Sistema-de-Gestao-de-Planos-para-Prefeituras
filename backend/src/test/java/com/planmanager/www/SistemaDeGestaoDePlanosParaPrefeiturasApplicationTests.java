516ac5a62343fa68dd9d7abccb0e3711c5f92f34
Iago Vitor <iagovls@gmail.com>
2026-03-13 04:31:34 -0300

    Update SistemaDeGestaoDePlanosParaPrefeiturasApplicationTests.java

--- backend/src/test/java/com/planmanager/www/SistemaDeGestaoDePlanosParaPrefeiturasApplicationTests.java
+++ backend/src/test/java/com/planmanager/www/SistemaDeGestaoDePlanosParaPrefeiturasApplicationTests.java
@@ -1,13 +1,63 @@
 package com.planmanager.www;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.planmanager.www.model.propostas.Proposta;
+import com.planmanager.www.model.planos.Plano;
+import com.planmanager.www.model.prefeituras.Prefeitura;
+import com.planmanager.www.repositories.PrefeituraRepository;
+import com.planmanager.www.repositories.PropostaRepository;
+import com.planmanager.www.repositories.PlanosRepository;
 
 @SpringBootTest
 class SistemaDeGestaoDePlanosParaPrefeiturasApplicationTests {
 
+	@Mock
+	PrefeituraRepository prefeituraRepository;
+
+	@Mock
+	PlanosRepository planoRepository;
+
+	@Mock
+	PropostaRepository propostaRepository;
+	
+	Prefeitura prefeitura = new Prefeitura();
+
+	Plano plano = new Plano();
+
+	@BeforeEach
+	void setUp() {
+
+		prefeitura.setId(1);
+		plano.setId(5);
+		plano.setPrefeitura(prefeitura);
+	}
+
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void validatePlanoBelongsToPrefeitura() {
+		
+
+		Proposta proposta = new Proposta();
+		proposta.setPlano(plano);
+		proposta.setPrefeitura(prefeitura);
+
+		
+		
+
 	}
 
 }
