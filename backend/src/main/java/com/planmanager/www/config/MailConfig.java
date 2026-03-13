546b4a9ce7bffe8483cd5bdd6be9f2d43186e522
Iago Vitor <iagovls@gmail.com>
2026-03-13 06:01:32 -0300

    Update MailConfig.java

--- backend/src/main/java/com/planmanager/www/config/MailConfig.java
+++ backend/src/main/java/com/planmanager/www/config/MailConfig.java
@@ -1,46 +1,46 @@
-package com.planmanager.www.config;
+// package com.planmanager.www.config;
 
-import java.util.Properties;
+// import java.util.Properties;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
+// import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.mail.javamail.JavaMailSender;
+// import org.springframework.mail.javamail.JavaMailSenderImpl;
 
-@Configuration
-public class MailConfig {
+// @Configuration
+// public class MailConfig {
 
-    @Value("${spring.mail.host}")
-    private String host;
+//     @Value("${spring.mail.host}")
+//     private String host;
 
-    @Value("${spring.mail.port}")
-    private Integer port;
+//     @Value("${spring.mail.port}")
+//     private Integer port;
 
-    @Value("${spring.mail.username}")
-    private String username;
+//     @Value("${spring.mail.username}")
+//     private String username;
 
-    @Value("${spring.mail.password}")
-    private String password;
+//     @Value("${spring.mail.password}")
+//     private String password;
 
-    @Value("${spring.mail.properties.mail.smtp.auth:true}")
-    private boolean smtpAuth;
+//     @Value("${spring.mail.properties.mail.smtp.auth:true}")
+//     private boolean smtpAuth;
 
-    @Value("${spring.mail.properties.mail.smtp.starttls.enable:true}")
-    private boolean startTls;
+//     @Value("${spring.mail.properties.mail.smtp.starttls.enable:true}")
+//     private boolean startTls;
 
-    @Bean
-    public JavaMailSender javaMailSender() {
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost(host);
-        mailSender.setPort(port);
-        mailSender.setUsername(username);
-        mailSender.setPassword(password);
+//     @Bean
+//     public JavaMailSender javaMailSender() {
+//         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+//         mailSender.setHost(host);
+//         mailSender.setPort(port);
+//         mailSender.setUsername(username);
+//         mailSender.setPassword(password);
 
-        Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.smtp.auth", String.valueOf(smtpAuth));
-        props.put("mail.smtp.starttls.enable", String.valueOf(startTls));
+//         Properties props = mailSender.getJavaMailProperties();
+//         props.put("mail.smtp.auth", String.valueOf(smtpAuth));
+//         props.put("mail.smtp.starttls.enable", String.valueOf(startTls));
 
-        return mailSender;
-    }
-}
+//         return mailSender;
+//     }
+// }
